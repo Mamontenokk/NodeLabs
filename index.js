12089466f2b1cdad36a496d537df24643c794309
Mamontenokk <vash1@ukr.net>
2019-03-10 16:36:55 +0200

    13

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+//const options = {
+//	key: fs.readFileSync('bad.key'),
+//	cert: fs.readFileSync('bad.pem')
+//};
+
+
+const server = http.createServer((request, response) =>{
+	console.log(request.url.split(':')[0]);
+	if(request.url.split(':')[0]==='http') {
+		http.get(request.url, (resp) =>{
+		resp.pipe(response);
+		}).on('error', (err)=>{
+			console.log('Error' + err.message);
+		});
+	}
+	if(request.url.split(':')[0]==='https') {
+		console.log(1);
+		https.get(request.url, (resp) => {
+		resp.pipe(response);
+		}).on('error', (err)=>{
+			console.log('Error' + err.message);
+		});
+	}
+	//response.end('hello world\n');
+});
+
+server.listen(5001);
