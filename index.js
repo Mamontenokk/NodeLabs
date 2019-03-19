6950809ee38024bf6d56d3cf913b8966ba20f537
Mamontenokk <vash1@ukr.net>
2019-03-19 21:09:48 +0200

    Another try

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-'use strict'; 
+'use strict';
 
 const http = require('http');
 const net = require('net');
-const url = require('url')
+const url = require('url');
 
 
 const server = http.createServer((request, response) =>{
 	console.log(request.url);
-
+	//parse URL
 	const ReqUrl = request.url;
 	const Result = ReqUrl.includes('//') ?
 		url.parse(ReqUrl) : url.parse('//' + ReqUrl, false, true);
 
 	response.writeHead(200, {'Content-Type': 'text/html'});
+	//Request to HTTP
 	if(Result.protocol==='http:') {
-		//response.addTrailers({'Dadadu': 'asdjasdj'})
-		//response.setHeader('asdasd', 'qasdasdasd');
 		http.get(request.url, (resp) =>{
 		resp.pipe(response);
+		//error handling
 		}).on('error', (err)=>{
 			console.log('Error' + err.message);
 		});
@@ -27,6 +27,7 @@ const server = http.createServer((request, response) =>{
 
 
 server.on('connect', (request, socket) =>{
+	//parse URL
 	const ReqUrl = request.url;
 	const Result = ReqUrl.includes('//') ?
 		url.parse(ReqUrl) : url.parse('//' + ReqUrl, false, true);
@@ -34,13 +35,15 @@ server.on('connect', (request, socket) =>{
 	const Connection = net.connect(Result.port || 443, Result.hostname, () => {
 		// tell the client that the connection is established
 		socket.write('HTTP/' + request.httpVersion + ' 200 OK\r\n\r\n', 'UTF-8', () => {
-			// creating pipes in both ends
+			//error handling
 			Connection.on('error', (err) => {
 				console.log('Error' + err.message);
 			});
+			// creating pipes in both ends
 			Connection.pipe(socket);
 			socket.pipe(Connection);
 		});
+		//error handling
 		socket.on('error', (err) => {
 			console.log('Error' + err.message);
 		});
