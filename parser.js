3042ea92d6594bdc7b93b300241e3d699d3ebd1d
Mamontenokk <vash1@ukr.net>
2019-06-13 14:33:48 +0300

    Refactoring

--- parser.js
+++ parser.js
@@ -8,11 +8,11 @@ const labels = {
     1: 'Route',
     2: 'Periodicity',
     3: 'Departure station',
-    4: 'Arrival time to d',
-    5: 'Departure time from d',
+    4: 'Arrival time to departure station',
+    5: 'Departure time from departure station',
     6: 'Arrival station',
-    7: 'Arrival time to a',
-    8: 'Departure time from a'
+    7: 'Arrival time to arrival station',
+    8: 'Departure time from arrival station'
 };
 
 function parse(fromStation, toStation){
@@ -41,24 +41,31 @@ function getRowData(row){
 }
 
 function formatSchedule(schedule){
+    const scheduleArray = [];
     let resultString = '';
     schedule.forEach((train, index)=>{
+        if((index+1)%5===0){
+            scheduleArray.push(resultString);
+            resultString=''
+        }
         resultString += `\n${index+1}.\n`;
         Object.keys(train).forEach(key=>{
             resultString += `${key} : ${train[key]} \n`
         })
     });
-    return resultString;
+    scheduleArray.push(resultString);
+    return scheduleArray;
 }
 
 const getSchedule = function getSchedule(inputMessage){ // inputMessage is a string, that has next form: city, city
-    const fromStation = inputMessage.split(',')[0];
-    const toStation = inputMessage.split(',')[1].trim();
+    const fromStation = inputMessage.split('-')[0];
+    const toStation = inputMessage.split('-')[1].trim();
     return parse(fromStation, toStation).then(result=>{
         return new Promise(resolve=>{
             resolve(formatSchedule(result))
         })
     })
 };
+
 
 module.exports = { getSchedule };
