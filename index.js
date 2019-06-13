656b6da8989fd3030f2c68f865c19da5d2a0f35a
Mamontenokk <vash1@ukr.net>
2019-06-13 14:37:07 +0300

    Webhooks support

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 const Telegraf  = require('telegraf');
 const {getSchedule} = require('./parser.js');
 
-const bot = new Telegraf('842567190:AAEPGv31awfyefzLVOyxE1Ye2LqVve6KFxU');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Print in your group name to get your schedule'));
 bot.hears(/\s*[А-ЯІа-яі]{1,40}\s*-\s*[А-ЯІа-яі]{1,40}\s*/, (ctx) => getSchedule(ctx.message.text)
     .then(result=>{result.forEach(page=>{ctx.reply(page)})})
     .catch(()=>ctx.reply('Ooopsie. Someone made an ooopsie')));
-bot.launch();
 
-/*bot.telegram.setWebhook('https://nodelabs-kpi-exam.mamontenok.now.sh');
+bot.telegram.setWebhook('https://nodelabs-kpi-exam.mamontenok.now.sh');
 
-module.exports = bot.webhookCallback('/');*/
+module.exports = bot.webhookCallback('/');
